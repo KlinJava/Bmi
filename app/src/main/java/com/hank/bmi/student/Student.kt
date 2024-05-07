004e43cf6684ace2d3997d0a384c3713e34e2bef
KlinJava <alarmpinglin@gmail.com>
2024-05-07 08:59:38 +0800

    Companion Object

--- app/src/main/java/com/hank/bmi/student/Student.kt
+++ app/src/main/java/com/hank/bmi/student/Student.kt
@@ -14,21 +14,54 @@ class Student(val id:String, val name:String, var english:Int, var math:Int) {  
     companion object {
         var test1 = 0
         var test2 = "ABC"
+        var pass = 60
     }
 
-    //第2個建構子
+    //第2個建構子，它可以使用「主建構子」的方式，將變數授與「初始值」，即可達到相同效果
+    // 如 class Student(val id:String, val name:String, var english:Int = 0, var math:Int = 0)
     constructor(id:String, name:String):this(id,name,0,0)
     fun print(){
-        println("$id\t$name\t$english\t$math")
+        val mark = if (average() < pass) "*" else ""
+        println("$id\t$name\t$english\t$math\t${average()}$mark\t${grading()}")
     }
+    fun average() = (english + math)/2
+
+    fun grading() = when(average()/10) {
+            9,10 -> "A"
+            8 -> "B"
+            7 -> "C"
+            6 -> "D"
+            else -> "F"
+        }
 }
 
 fun main() {
-    val stu1 = Student("001","jack",70,80)
+    Student.pass = 50
+
+    //使用 list方式
+    val students = listOf<Student>(
+        Student("001","jack",95,99),
+        Student("002","Hank",66,87),
+        Student("003","Jane")
+    )
+    //for，由 list決定長度
+    for (stu in students) {
+        stu.print()
+    }
+
+    println()
+    //for，自已設定範圍
+    for (i in 0..2) {
+        students.get(i).print()
+    }
+    println()
+    val stu1 = Student("001","jack",95,99)
     val stu2 = Student("002","Hank",66,87)
     val stu3 = Student("003","Jane")
     stu1.print()
     stu2.print()
     stu3.print()
+    println()
+
 
 }
